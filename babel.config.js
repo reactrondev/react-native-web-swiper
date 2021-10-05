49db3b4ceb8ae6bf3af1247c689e4e8067fa5a90
Achmad Mahardi <achmad@mahardi.me>
2021-10-05 10:53:42 +0700

    FEAT: build commonjs flavor

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,15 @@
 module.exports = function(api) {
     api.cache(true);
     return {
-        presets: ["minify"],
+        presets: [
+          ["@babel/preset-env",
+          {
+            targets: {
+              node: 14,
+              esmodules: false
+            }
+          }
+        ]],
         plugins: [
           "@babel/plugin-transform-react-jsx",
           "@babel/plugin-proposal-class-properties"
